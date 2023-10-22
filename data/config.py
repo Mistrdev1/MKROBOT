a864f5ac2e4a421612a2766d196ba5e8d45d7722
Mistrdev1 <146039305+Mistrdev1@users.noreply.github.com>
2023-10-22 06:39:05 +0500

    updated

--- data/config.py
+++ data/config.py
@@ -5,10 +5,10 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN_TEST")  # Bot toekn
-ADMINS = [1849953640]
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = [1849953640]
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
 DEVELOPER = env.str("DEVELOPER")
-# CHANNELS = [-1001569759901,]
-CHANNELS = [-1001208742794,]
+CHANNELS = [-1001569759901,]
+# CHANNELS = [-1001208742794,]
